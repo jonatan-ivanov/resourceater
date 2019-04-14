72d1b604759651a3149af3b486584478699a93b9
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2019-04-14 00:45:52 -0700

    DaemonThreadResourceRequest implements Request

--- src/main/java/resourceater/model/resource/thread/daemon/DaemonThreadResourceRequest.java
+++ src/main/java/resourceater/model/resource/thread/daemon/DaemonThreadResourceRequest.java
@@ -1,11 +1,12 @@
 package resourceater.model.resource.thread.daemon;
 
 import lombok.Value;
+import resourceater.model.resource.Request;
 
 /**
  * @author Jonatan Ivanov
  */
 @Value
-public class DaemonThreadResourceRequest {
+public class DaemonThreadResourceRequest implements Request {
     private final int size;
 }
