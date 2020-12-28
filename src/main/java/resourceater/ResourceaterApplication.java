88c6620af4c65b011ba30b7f1da10e1e6e88dcd2
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2020-12-27 17:53:14 -0800

    Upgrading dependencies

--- src/main/java/resourceater/ResourceaterApplication.java
+++ src/main/java/resourceater/ResourceaterApplication.java
@@ -5,13 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * @author Jonatan Ivanov
  */
 @EnableFeignClients
-@EnableSwagger2WebMvc
 @EnableHypermediaSupport(type = HypermediaType.HAL)
 @SpringBootApplication
 public class ResourceaterApplication {
