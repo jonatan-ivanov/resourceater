e8044c015180ece40a95b45bd5247d6d07d0b686
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2022-12-15 18:15:30 -0800

    Add age to the model

--- src/main/java/resourceater/model/resource/Model.java
+++ src/main/java/resourceater/model/resource/Model.java
@@ -2,8 +2,10 @@ package resourceater.model.resource;
 
 import java.time.Duration;
 import java.time.Instant;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.hateoas.RepresentationModel;
 
 /**
@@ -14,12 +16,14 @@ import org.springframework.hateoas.RepresentationModel;
 public abstract class Model<R extends Resource<R>> extends RepresentationModel<Model<R>> {
     protected final String id;
     protected final Duration ttl;
+    protected final Duration age;
     protected final Instant createdAt;
     protected final Instant diesAt;
 
     public Model(R resource){
         this.id = resource.getId();
         this.ttl = resource.getTtl().orElse(null);
+        this.age = resource.getAge();
         this.createdAt = resource.getCreatedAt();
         this.diesAt = resource.getDiesAt();
     }
